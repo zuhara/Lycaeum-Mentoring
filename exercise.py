23ca0550d7c84b79478588030aabcb61e768246a
zuhara <zuhara@hamon.in>
2019-12-03 15:47:49 +0530

    Second Commit

---
+++ exercise.py
@@ -0,0 +1,25 @@
+# Example warm up programs
+ 
+# Q1
+def digits(n):
+    "Returns number of digits in number n"
+    return len(str(n))
+
+# The len() function can be used to get the length of any iterable
+
+
+# Q2
+def words(s):
+    "Returns number of words in sentence s"
+    return len(s.split(" "))
+
+# Q3
+def bigger(a,b):
+    if (a > b):
+        return a
+    else:
+        return b
+
+
+            
+
