2541c6cb26237bcadd35e02ad73b073ce0b9bc57
zuhara <zuhara@hamon.in>
2019-12-04 12:06:25 +0530

    Tables examples

--- exercise.py
+++ exercise.py
@@ -19,7 +19,7 @@ def bigger(a,b):
         return a
     else:
         return b
-
+# Q4
 def biggest(a,b,c):
     if (a > b) and (a > c):
         return a
@@ -28,4 +28,42 @@ def biggest(a,b,c):
     else:
         return c
             
+
+# def biggest1(a,b,c):
+#     if a > b:
+#         temp = a
+#     if temp > c:
+#         temp = c
+
+
+# Q5
+def tables2(x):
+    for i in range(1,x+1):
+        for j in range(1,x+1):
+            print("{:3}".format(i*j),end=' ')
+        print("\n")
+        
+# Q6
+def tables3(x):
+    for i in range(1,x+1):
+        for j in range(1,x+1):
+            print(j,end=' ')
+        print("\n")
+
+# Q7
+def tables4(x):
+    for i in range(1,x+1):
+        for j in range(1,x+1):
+           # print("____",end=' ')
+            print("{:3}".format(i*j),end=' ')
+           # print("____",end=' ')
+        print("\n")
+        print("__________")
+
+# Q8
+def tables5(x):
+    for i in  range(1,x+1):
+        for j in range(1,x+1):
+            print("{:3}".format(1),end=' ')
+        print("\n")
 
