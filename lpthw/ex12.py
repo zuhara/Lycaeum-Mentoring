9bc2bd16fa0024f917e554b879c026d03d9a7c0c
zuhara <zuhara@hamon.in>
2019-12-04 12:07:27 +0530

    Exercises done from 1 to 12

---
+++ lpthw/ex12.py
@@ -0,0 +1,7 @@
+age = input("How old are you ? ")
+height = input("Whats your height ? ")
+weight = input("Whats your weight ? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("So, you're {} old, {} tall and {} heavy.".format(age,height,weight))
---
+++ lpthw/ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Zuhara Sharin'
+my_age = 31 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"She's got {my_eyes} eyes and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
