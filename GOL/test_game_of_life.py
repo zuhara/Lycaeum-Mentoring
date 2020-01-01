995daddb600938ae3c3012b032edc900b57455e8
balabg94 <balagovind2094@gmail.com>
2020-01-01 15:41:05 +0530

    More than two seed in initial state ---->GOL

--- GOL/test_game_of_life.py
+++ GOL/test_game_of_life.py
@@ -23,3 +23,27 @@ def test_one_initial_state():
          [False , False , False , False , False , False , False , False],
          [False , False , False , False , False , False , False , False],]
     assert a == e
+
+def test_two_initial_state():
+    a = game_of_life.function1(p = [[3,3],[1,2]])
+    e = [[False , False , False , False , False , False , False , False],
+         [False , False , True , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , True , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],]
+    assert a == e
+
+def test_many_initial_state():
+    a = game_of_life.function1(p = [[3,3],[1,2],[2,2],[2,3]])
+    e = [[False , False , False , False , False , False , False , False],
+         [False , False , True , False , False , False , False , False],
+         [False , False , True , True , False , False , False , False],
+         [False , False , False , True , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],
+         [False , False , False , False , False , False , False , False],]
+    assert a == e
